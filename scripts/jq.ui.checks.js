803fb6a851c5dc07499176fb9ef93a6a5723f694
Kazunori Tokuda <norimania@gmail.com>
2012-04-02 23:09:38 +0900

    jq.ui.checks 0.1

--- scripts/jq.ui.checks.js
+++ scripts/jq.ui.checks.js
@@ -1,219 +1,307 @@
-(function() {
-  var __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+/*!
+ * jq.ui.checks
+ *
+ * @version      0.1
+ * @author       nori (norimania@gmail.com)
+ * @copyright    5509 (http://5509.me/)
+ * @license      The MIT License
+ * @link         https://github.com/5509/jq.ui.checks
+ *
+ * 2012-04-02 23:08
+ */
+(function($, window, document) {
 
-  (function($, window, document) {
-    var Checkbox, Checks, Radio;
-    Checks = (function() {
-
-      function Checks($elems, conf) {
-        this.namespace = 'Checks';
-        if (!(this instanceof Checks)) return new Checks($elems, conf);
-        return this.init($elems, conf);
+  var __hasProp, __extends;
+  __hasProp = Object.prototype.hasOwnProperty;
+  __extends = function(child, parent) {
+    function F() {
+      this.constructor = child;
+    }
+    for ( var key in parent ) {
+      if ( __hasProp.call(parent, key) ) {
+        child[key] = parent[key];
       }
+    }
+    F.prototype = parent.prototype;
+    child.prototype = new F();
+    child.__super__ = parent.prototype;
+    return child;
+  };
 
-      Checks.prototype.conf = {
-        type: 'checkbox',
-        uiClass: 'ui_check',
-        labelClass: 'ui_check_label',
-        checkedClass: 'ui_checked',
-        disabledClass: 'ui_check_disabled'
-      };
+  // Based
+  function Checks($elems, conf) {
+    this.namespace = 'Checks';
+    if ( !(this instanceof Checks) ) {
+      return new Checks($elems, conf);
+    }
+    return this.init($elems, conf);
+  }
+  Checks.prototype = {
 
-      Checks.prototype.elemMaps = {};
+    // names and types are based
+    conf: {
+      uiClass: 'ui_check',
+      labelClass: 'ui_check_label',
+      checkedClass: 'ui_checked',
+      disabledClass: 'ui_check_disabled'
+    },
 
-      Checks.prototype.init = function($elems, conf) {
-        var _this = this;
-        this.conf = $.extend(this.conf, conf);
-        $.each($elems, function(i, elem) {
-          var $check, $label, $view, check;
-          check = _this._matching(elem);
-          $check = check.$check;
-          $label = check.$label;
+    init: function($elems, conf) {
+      var self = this;
+
+      self.$elems = $elems;
+      self.conf = $.extend(self.conf, conf);
+      self.elemMaps = {};
+
+      $.each($elems, function(i, elem) {
+        var check = self._matching(elem),
+          $check = check.$check,
+          $label = check.$label,
           $view = check.$view;
-          if ($label) $label.addClass(_this.conf.labelClass);
-          $check.hide().after($view);
-        });
-        console.log(this.elemMaps);
-        this._eventify();
-      };
 
-      Checks.prototype._view = function(check) {
-        var $view;
+        if ( $label ) {
+          $label.addClass(self.conf.labelClass);
+        }
+        $check.hide().after($view);
+      });
+
+      // manage events
+      self._eventify();
+    },
+
+    _view: function(check) {
+      var self = this,
         $view = $('<span></span>').addClass(this.conf.uiClass);
-        if (check.disabled) $view.addClass(this.conf.disabledClass);
-        if (check.checked) $view.addClass(this.conf.checkedClass);
-        return $view;
-      };
+      if ( check.disabled ) {
+        $view.addClass(self.conf.disabledClass);
+      }
+      if ( check.checked ) {
+        $view.addClass(self.conf.checkedClass);
+      }
+      return $view;
+    },
 
-      Checks.prototype._matching = function(check) {
-        var id;
+    _matching: function(check) {
+      var self = this,
         id = check.id;
-        this.elemMaps[id] = {
-          id: id,
-          $check: $(check),
-          $view: this._view(check),
-          $label: $('label[for=' + id + ']'),
-          state: check.checked,
-          disabled: check.disabled
-        };
-        return this.elemMaps[id];
+      self.elemMaps[id] = {
+        id: id,
+        $check: $(check),
+        $view: self._view(check),
+        $label: $('label[for=' + id + ']'),
+        state: check.checked,
+        disabled: check.disabled
       };
+      return self.elemMaps[id];
+    },
 
-      Checks.prototype._eventify = function() {
-        var _this = this;
-        $.each(this.elemMaps, function(key, val) {
-          var $check, $label, $view, disabled, id;
-          id = val.id;
-          $check = val.$check;
-          $label = val.$label;
-          $view = val.$view;
+    _eventify: function() {
+      var self = this;
+      // binding events to all checkboxes
+      $.each(self.elemMaps, function(key, val) {
+        var id = val.id,
+          $check = val.$check,
+          $label = val.$label,
+          $view = val.$view,
           disabled = val.disabled;
-          $check.bind({
-            'click': function(ev) {
-              ev.stopPropagation();
-              if (disabled) return;
-              $check.trigger('check:toggle');
-            },
-            'check:toggle': function(ev) {
-              if (!$view.hasClass(_this.conf.checkedClass)) {
-                _this._checkOn(id);
-              } else {
-                _this._checkOff(id);
-              }
-            },
-            'check:on': function() {
-              return _this._checkOn(id);
-            },
-            'check:off': function() {
-              return _this._checkOff(id);
+
+        // binding events
+        $check.bind({
+          'click': function(ev) {
+            ev.stopPropagation();
+            if ( disabled ) {
+              return;
             }
-          });
-          if (!$.support.opacity && $label.length) {
-            return $label.click(function(ev) {
-              ev.stopPropagation();
-              $check.trigger('click');
-            });
+            $check.trigger('check:toggle');
+          },
+          'check:toggle': function(ev) {
+            if ( !$view.hasClass(self.conf.checkedClass) ) {
+              self._checkOn(id);
+            } else {
+              self._checkOff(id);
+            }
+          },
+          'check:on': function() {
+            self._checkOn(id);
+          },
+          'check:off': function() {
+            self._checkOff(id);
+          },
+          'check:enabled': function() {
+            self._enable(id);
+          },
+          'check:disabled': function() {
+            self._disable(id);
           }
         });
-      };
 
-      Checks.prototype._checkOn = function(id) {
-        var map;
-        map = this.elemMaps[id];
-        map.$view.addClass(this.conf.checkedClass);
-        map.$check.prop('checked', 'checked');
-      };
-
-      Checks.prototype._checkOff = function(id) {
-        var map;
-        map = this.elemMaps[id];
-        map.$view.removeClass(this.conf.checkedClass);
-        map.$check.prop('checked', '');
-      };
-
-      Checks.prototype._callAPI = function(api, args) {
-        if (typeof this[api] !== 'function') {
-          throw new Error(api + ' does not exist of ' + this.namespace + ' methods.');
-        } else if (/^_/.test(api) && typeof this[api] === 'function') {
-          throw new Error('Method begins with an underscore are not exposed.');
+        // less than IE8 and $label exists
+        if ( !$.support.opacity && $label.length ) {
+          $label.click(function(ev) {
+            ev.stopPropagation();
+            $check.trigger('click');
+          });
         }
-        return this[api](args);
-      };
+      });
+    },
 
-      Checks.prototype.enable = function() {};
+    _checkOn: function(id) {
+      var self = this,
+        map = self.elemMaps[id];
 
-      Checks.prototype.enableAll = function() {};
+      map.$view.addClass(self.conf.checkedClass);
+      map.$check.prop('checked', 'checked');
+    },
 
-      Checks.prototype.disable = function() {};
+    _checkOff: function(id) {
+      var self = this,
+        map = self.elemMaps[id];
 
-      Checks.prototype.disableAll = function() {};
+      map.$view.removeClass(self.conf.checkedClass);
+      map.$check.prop('checked', '');
+    },
 
-      Checks.prototype.destroy = function() {};
+    _enable: function(id) {
+      var self = this,
+        map = self.elemMaps[id],
+        $view = map.$view,
+        $check = map.$check;
 
-      return Checks;
+      $view.addClass(self.conf.disabledClass);
+      $check.prop('disabled', '');
+    },
 
-    })();
-    Checkbox = (function(_super) {
+    _disable: function(id) {
+      var self = this,
+        map = self.elemMaps[id],
+        $view = map.$view,
+        $check = map.$check;
 
-      __extends(Checkbox, _super);
+      $view.removeClass(self.conf.disabledClass);
+      $check.prop('disabled', 'disabled');
+    },
 
-      function Checkbox($elems, conf) {
-        this.namespace = 'Checkbox';
-        if (!(this instanceof Checkbox)) return new Checkbox($elems, conf);
-        return this.init($elems, conf);
+    _callAPI: function(api, args) {
+      var self = this;
+
+      if ( typeof self[api] !== 'function' ) {
+        throw new Error(api + ' does not exist of ' + self.namespace + ' methods.');
+      } else
+      if ( /^_/.test(api) && typeof self[api] === 'function' ) {
+        throw new Error('Method begins with an underscore are not exposed.');
       }
+      return self[api](args);
+    },
 
-      Checkbox.prototype.checkOn = function(id) {
-        this._checkOn(id);
-      };
+    checkOn: function(id) {
+      var self = this;
+      self._checkOn(id);
+    },
 
-      Checkbox.prototype.checkOff = function(id) {
-        this._checkOff(id);
-      };
+    checkOff: function(id) {
+      var self = this;
+      self._checkOff(id);
+    },
 
-      Checkbox.prototype.checkOnAll = function() {
-        $.each(this.elemMaps, function(key, val) {
-          return val.$check.trigger('check:on');
-        });
-      };
+    enable: function(id) {
+      var self = this;
+      self._enable(id);
+    },
+    enableAll: function() {
+      var self = this;
+      $.each(self.elemMaps, function(key, val) {
+        self.enable(val.id);
+      });
+    },
 
-      Checkbox.prototype.checkOffAll = function() {
-        $.each(this.elemMaps, function(key, val) {
-          return val.$check.trigger('check:off');
-        });
-      };
+    disable: function(id) {
+      var self = this;
+      self._disable(id);
+    },
+    disableAll: function() {
+      var self = this;
+      $.each(self.elemMaps, function(key, val) {
+        self.disable(val.id);
+      });
+    },
 
-      return Checkbox;
+    destroy: function() {}
+  };
 
-    })(Checks);
-    Radio = (function(_super) {
+  // Checkbox
+  var cp;
+  __extends(Checkbox, Checks);
+  function Checkbox($elems, conf) {
+    this.namespace = 'Checkbox';
+    if ( !(this instanceof Checkbox) ) {
+      return new Checkbox($elems, conf);
+    }
+    return this.init($elems, conf);
+  };
+  cp = Checkbox.prototype;
+  cp.toggleCheckAll = function() {
+    var self = this;
+    self.$elems.trigger('check:toggle');
+  };
+  cp.checkOnAll = function() {
+    var self = this;
+    self.$elems.trigger('check:on');
+  };
+  cp.checkOffAll = function() {
+    var self = this;
+    self.$elems.trigger('check:off');
+  };
 
-      __extends(Radio, _super);
+  // Radio
+  var rp;
+  __extends(Radio, Checks);
+  function Radio($elems, conf) {
+    this.namespace = 'Radio';
+    if ( !(this instanceof Radio) ) {
+      return new Radio($elems, conf);
+    }
+    return this.init($elems, conf);
+  };
+  rp = Radio.prototype;
+  rp.conf = {
+    uiClass: 'ui_radio',
+    labelClass: 'ui_radio_label',
+    checkedClass: 'ui_checked',
+    disabledClass: 'ui_radio_disabled'
+  };
+  rp._checkOn = function(id) {
+    var self = this,
+      map = self.elemMaps[id];
 
-      function Radio($elems, conf) {
-        this.namespace = 'Radio';
-        if (!(this instanceof Radio)) return new Radio($elems, conf);
-        return this.init($elems, conf);
-      }
+    $.each(self.elemMaps, function(key, val) {
+      self._checkOff(val.id);
+    });
+    map.$view.addClass(self.conf.checkedClass);
+    map.$check.prop('checked', 'checked');
+  };
 
-      Radio.prototype.conf = {
-        type: 'radio',
-        uiClass: 'ui_radio',
-        labelClass: 'ui_radio_label',
-        checkedClass: 'ui_checked',
-        disabledClass: 'ui_radio_disabled'
-      };
+  // extends $.fn
+  $.fn.checkbox = function(a, b) {
+    var checkbox;
+    checkbox = this.data('checkbox');
+    if (checkbox) {
+      return checkbox._callAPI(a, b);
+    } else {
+      checkbox = Checkbox(this, a);
+      this.data('checkbox', checkbox);
+      return this;
+    }
+  };
+  $.fn.radio = function(a, b) {
+    var radio;
+    radio = this.data('radio');
+    if (radio) {
+      return radio._callAPI(a, b);
+    } else {
+      radio = Radio(this, a);
+      this.data('radio', radio);
+      return this;
+    }
+  };
 
-      Radio.prototype.checkOn = function() {};
-
-      Radio.prototype.checkOff = function() {};
-
-      return Radio;
-
-    })(Checks);
-    $.fn.checkbox = function(a, b) {
-      var checkbox;
-      checkbox = this.data('checkbox');
-      if (checkbox) {
-        return checkbox._callAPI(a, b);
-      } else {
-        checkbox = Checkbox(this, a);
-        this.data('checkbox', checkbox);
-        return this;
-      }
-    };
-    $.fn.radio = function(a, b) {
-      var radio;
-      radio = this.data('radio');
-      if (radio) {
-        return radio._callAPI(a, b);
-      } else {
-        radio = Radio(this, a);
-        this.data('radio', radio);
-        return this;
-      }
-    };
-  })(jQuery, this, this.document);
-
-}).call(this);
+}(jQuery, this, this.document));
